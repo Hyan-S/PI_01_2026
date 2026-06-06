b9d02c399f2ff6cbf5fcbda95bd310bbf1aeff02
Hyan Salas Da Silva Costa <hyansalas@gmail.com>
2026-06-05 22:27:21 -0300

    feat:(backend): adicionar entidade Equipamento (#3)

---
+++ BackEnd/Senai/Senai/src/main/java/pi/Senai/Senai/entity/Equipamento.java
@@ -0,0 +1,103 @@
+package pi.Senai.Senai.entity;
+
+import java.sql.Date;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Equipamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID Id;
+
+    @Column(nullable = false)
+    private String Nome; 
+
+    @Column
+    private String Historico;
+
+    @Column
+    private String Observacao;
+
+    //Implementação futura
+    @Column
+    private String IdEquipe;
+
+    @Column
+    private Date DataCriacao;
+
+    @Column
+    private Date UltimaAtualizacao;
+
+    @Column
+    private Boolean Ativo;
+
+    public UUID getId() {
+        return Id;
+    }
+
+    public void setId(UUID id) {
+        Id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public String getHistorico() {
+        return Historico;
+    }
+
+    public void setHistorico(String historico) {
+        Historico = historico;
+    }
+
+    public String getObservacao() {
+        return Observacao;
+    }
+
+    public void setObservacao(String observacao) {
+        Observacao = observacao;
+    }
+
+    public String getIdEquipe() {
+        return IdEquipe;
+    }
+
+    public void setIdEquipe(String idEquipe) {
+        IdEquipe = idEquipe;
+    }
+
+    public Boolean getAtivo() {
+        return Ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        Ativo = ativo;
+    }
+
+    public Date getDataCriacao() {
+        return DataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        DataCriacao = dataCriacao;
+    }
+
+    public Date getUltimaAtualizacao() {
+        return UltimaAtualizacao;
+    }
+
+    public void setUltimaAtualizacao(Date ultimaAtualizacao) {
+        UltimaAtualizacao = ultimaAtualizacao;
+    }
+}
