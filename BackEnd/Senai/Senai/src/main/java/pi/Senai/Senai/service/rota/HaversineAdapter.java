01e896de492ccd325ae49ec336515580f6e07ecd
Hatus <hatusgts@gmail.com>
2026-06-11 20:07:09 -0300

    refactor(rota): adiciona ServicoDeGeolocalizacao para demonstrar duas interfaces no padrão Adapter

--- BackEnd/Senai/Senai/src/main/java/pi/Senai/Senai/service/rota/HaversineAdapter.java
+++ BackEnd/Senai/Senai/src/main/java/pi/Senai/Senai/service/rota/HaversineAdapter.java
@@ -6,10 +6,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class HaversineAdapter implements CalculadorDeDistancia {
 
-    private final HaversineCalculador calculador = new HaversineCalculador();
+    private final ServicoDeGeolocalizacao servico = new HaversineCalculador();
 
     @Override
     public double calcular(double latOrigem, double lonOrigem, double latDestino, double lonDestino) {
-        return calculador.calcularDistancia(latOrigem, lonOrigem, latDestino, lonDestino);
+        Coordenada origem = new Coordenada(latOrigem, lonOrigem);
+        Coordenada destino = new Coordenada(latDestino, lonDestino);
+        return servico.calcularDistanciaEntrePontos(origem, destino);
     }
 }
---
+++ BackEnd/Senai/Senai/src/main/java/pi/Senai/Senai/service/rota/ServicoDeGeolocalizacao.java
@@ -0,0 +1,6 @@
+package pi.Senai.Senai.service.rota;
+
+public interface ServicoDeGeolocalizacao {
+
+    double calcularDistanciaEntrePontos(Coordenada pontoA, Coordenada pontoB);
+}
